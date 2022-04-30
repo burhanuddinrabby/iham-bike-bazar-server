88fe5bc44f7dcaed8edb83b345ef1dd4e51fc800
Burhan Uddin <bunnub5683@gmail.com>
2022-04-30 17:37:20 +0600

    basic delete operation added

--- index.js
+++ index.js
@@ -60,6 +60,15 @@ async function run() {
             res.send(result);
         })
 
+        //deleting a product
+        //deleting a user
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            // const result = await productCollection.deleteOne(query);
+            // res.send(result);
+        })
 
     } finally {
         //   await client.close();
